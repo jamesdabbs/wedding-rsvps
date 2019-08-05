f38ee34807c7cdfadc05c00afca5595553235712
James Dabbs <jamesdabbs@gmail.com>
2019-08-05 14:40:08 -0700

    Add response mailer

--- app/mailers/rsvp_mailer.rb
+++ app/mailers/rsvp_mailer.rb
@@ -1,4 +1,19 @@
 class RsvpMailer < ApplicationMailer
   def rsvp(rsvp)
+    @rsvp = rsvp
+
+    @message = if rsvp.any_attending?
+      "We can't wait to see you in October!"
+    else
+      "We're sorry to hear you can't make it, but we'll look forward to catching up soon!"
+    end
+
+    mail(
+      to:       @rsvp.email,
+      from:     'rsvp@rachelandjames.wedding',
+      cc:       ['james.dabbs+rsvp@gmail.com', 'resullivan12+rsvp@gmail.com'],
+      reply_to: ['resullivan12+rsvp@gmail.com'],
+      subject:  "RSVP for Rachel & James' Wedding"
+    )
   end
 end
--- app/models/rsvp.rb
+++ app/models/rsvp.rb
@@ -1,11 +1,15 @@
 class Rsvp < ApplicationRecord
-  has_many :pluses, class_name: 'RsvpPlus'
+  has_many :pluses, class_name: 'RsvpPlus', dependent: :destroy
 
   validates :name, presence: true
   validates :email, presence: true
   validates :attending, presence: true
 
+  def any_attending?
+    attending == 'yes' || pluses.any? { |plus| plus.attending == 'yes' }
+  end
+
   def deliver
-    # RsvpMailer.rsvp(self).deliver_now
+    RsvpMailer.rsvp(self).deliver_now
   end
 end
